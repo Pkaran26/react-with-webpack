ebfdb6ea93599d3a91cf37d52e82d4bc25c6678e
Prateek Kaushik <prateek@numeroeins.com>
2020-06-19 07:57:05 +0530

    webpack with koa

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Header from './header'
+import koa from 'koa'
+import http from 'http'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+import Router from 'koa-router'
+import { createReadStream } from 'fs'
+import serve from 'koa-static'
+import webpack from 'webpack'
+import webpackMiddleware from "koa-webpack-dev-middleware"
 
-const App = ()=>(
-  <div>
-    <Header />
-    <h1>hello</h1>
-  </div>
-)
+const app = new koa()
+const server = http.createServer(app.callback())
+server.listen(8181)
+console.log('Server running...')
 
-render(<App />, document.getElementById("app"))
+app.use(bodyParser())
+app.use(cors())
+app.use(serve(__dirname + '/public'));
+
+app.use(webpackMiddleware(webpack({
+    entry: __dirname+"/front/index.js",
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader"
+        },
+        //{ test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      ]
+    },
+    output: {
+      path:  __dirname+"/public",
+      publicPath: "http://localhost:8181/",
+      filename: "bundle.js"
+    }
+})));
+
+const router = new Router({ prefix: '' })
+
+router.get('/', ctx=>{
+  ctx.type = 'html';
+  ctx.body = createReadStream(`${ __dirname }/public/index.html`)
+})
+
+app.use(router.routes()).use(router.allowedMethods())
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,22 @@ const path = require("path")
 
 module.exports = {
   devServer: {
-    contentBase: path.resolve(__dirname, "./src"),
+    contentBase: path.resolve(__dirname, "./src/front"),
     historyApiFallback: true
   },
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, "./src/front/index.js"),
   module: {
     rules: [
       {
         test: /\.js$/,
         use: "babel-loader"
-      }
+      },
+      //{ test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
     ]
   },
   output: {
+    path: path.join(__dirname, './src/public'),
+    publicPath: "http://localhost:8181/",
     filename: "bundle.js"
   }
 }
